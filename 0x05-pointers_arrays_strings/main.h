7dd05b07f5af977c866c684e6cacba380f1d7ee8
Dexymaroni <divineinyang7@gmail.com>
2022-07-12 16:31:44 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,8 +2,8 @@ int _putchar(char c);
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
+int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
@@ -11,4 +11,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
