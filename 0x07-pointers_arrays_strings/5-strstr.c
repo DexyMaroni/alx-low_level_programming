2569885b536572ac3697d7158dfa5e436fa87a0a
Dexymaroni <divineinyang7@gmail.com>
2022-07-18 16:04:21 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to be scanned
- * @accept: string containing characters to match
- * Return: number of matching characters in the initial segment
+ * _strstr - function locate
+ * @haystack: pointer to char
+ * @needle: pointer to char
+ * Return: 0
  */
+char *_strstr(char *haystack, char *needle)
+{
+char *result = haystack, *fneedle = needle;
 
-unsigned int _strspn(char *s, char *accept)
+while (*haystack)
 {
-int i, j, flag;
-
-int count = 0;
-
-for (i = 0; s[i] != '\0'; i++)
+while (*needle)
 {
-flag = 0;
-for (j = 0; accept[j] != '\0'; j++)
+if (*haystack++ != *needle++)
 {
-if (s[i] == accept[j])
-{
-count++;
-flag = 1;
+break;
 }
 }
-if (flag == 0)
-return (count);
+if (!*needle)
+{
+return (result);
+}
+needle = fneedle;
+result++;
+haystack = result;
 }
 
 return (0);
